aeaab54579ea1b0b444cab78f29a23cb216562a6
tkvekvetsia <kvekvetsiatoma@gmail.com>
2022-08-29 17:15:51 -0700

    update login and register UI

--- project/src/app/core/components/login/login.component.ts
+++ project/src/app/core/components/login/login.component.ts
@@ -1,16 +1,33 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ILoginForm } from 'src/app/shared/itnerfaces/login.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
+
+  //login form
+  loginForm: FormGroup<ILoginForm> = new FormGroup({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(8)],
+    }),
+  });
+
+  public onLogin(): void {}
+
+
 
 }
